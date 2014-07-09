77af18b4aaf277e37f94456fc71c377e64ba329f
Mike Fadul <mikefadul@yaho.com>
2014-07-09 19:49:31 -0400

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,8 +12,15 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
        page.should have_selector('title', :text => 
-      				'Ruby on Rails Tutorial Sample App | Home')
+      				'Ruby on Rails Tutorial Sample App')
     end
+
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+       page.should_not have_selector('title', :text => '| Home')
+    end
+
+
   end
 
   describe "Help page" do
